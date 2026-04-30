60fb0a43bc698256e10e57d0d0a212aedd6e2710
Muhammad Abdul Moeed <muhammadabdulmoeed.96@gmail.com>
2026-04-30 12:28:49 +0200

    correct hidden size dimension in decoder conv layer

--- pdetransformer/core/mixed_channels/pde_transformer.py
+++ pdetransformer/core/mixed_channels/pde_transformer.py
@@ -1877,7 +1877,10 @@ class PDEImpl(nn.Module):
                 self.__setattr__(f"up{i+1}_{i}", Upsample(hidden_size_upsample, keep_dim=keep_dim))
             else:
                 self.__setattr__(f"up{i + 1}_{i}", Upsample_FAKE(hidden_size_upsample, keep_dim=keep_dim))
-            self.__setattr__(f"reduce_chan_level{i}", nn.Conv2d(hidden_size_layer * 2, hidden_size_layer, kernel_size=1, bias=True))
+            
+            self.__setattr__(f"reduce_chan_level{i}", nn.Conv2d(hidden_size_upsample, hidden_size_layer, kernel_size=1, bias=True)) # EDIT: moeed. hidden_size_upsample is the correct hidden size
+            # self.__setattr__(f"reduce_chan_level{i}", nn.Conv2d(hidden_size_layer * 2, hidden_size_layer, kernel_size=1, bias=True))
+
             self.__setattr__(f"decoder_level_{i}", PDEStage(dim=hidden_size_layer, num_heads=num_heads,
                                             window_size=window_size, depth=depth[self.num_encoder_layers - i - 1], **dit_stage_args))
 
